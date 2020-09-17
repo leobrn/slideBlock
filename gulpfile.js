a47e1da369db66082ef25034a416d99e5bf2d3fe
Очаковский Владимир Михайлович <Ochakovskiy.V@ssa.ru>
2020-09-17 11:22:19 +0700

    Добавление min.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     cssbeautify = require('gulp-cssbeautify'),
     ghPages = require('gh-pages'),
-    path = require('path')
+    path = require('path'),
+    minify = require('gulp-minify')
 
 gulp.task('browser-sync', () => {
     browserSync.init({
@@ -16,12 +17,23 @@ gulp.task('browser-sync', () => {
 gulp.task('html', () => {
     return gulp.src('app/*.html')
         .pipe(browserSync.stream())
-});
+})
 
 gulp.task('js', () => {
     return gulp.src('app/js/*.js')
         .pipe(browserSync.stream())
-});
+})
+
+gulp.task('minjs', () => {
+    return gulp.src('app/js/slideBlock.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+            ignoreFiles: ['-min.js']
+        }))
+        .pipe(gulp.dest('app/js/'))
+})
 
 gulp.task('less', () => {
     return gulp.src(['app/less/**/*.less', '!app/less/**/_*.less'])
@@ -29,7 +41,11 @@ gulp.task('less', () => {
         .pipe(cssbeautify())
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream())
-});
+})
+
+gulp.task('deploy', function (cb) {
+    ghPages.publish(path.join(process.cwd(), '/app'), cb);
+})
 
 gulp.task('watch', () => {
     gulp.watch('app/less/**/*.less', gulp.parallel('less'))
@@ -37,6 +53,4 @@ gulp.task('watch', () => {
     gulp.watch('app/**/*.js', gulp.parallel('js'))
 })
 
-gulp.task('deploy', function(cb)  {
-    ghPages.publish(path.join(process.cwd(), '/app'), cb);
-})
+gulp.task('default', gulp.parallel('browser-sync', 'watch'))
