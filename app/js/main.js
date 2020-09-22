51f8ffb6e4ff21fc2d98f08786e6a1ea56b36654
Очаковский Владимир Михайлович <Ochakovskiy.V@ssa.ru>
2020-09-22 11:51:45 +0700

    Добавлен функционал для модального окна и реализованы примеры

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,60 @@
+const btnsModal = document.querySelectorAll('[data-modalComplex]'),
+    modalItems = []
+
+function setState(item) {
+
+    const index = item.dataset.item - 1,
+        obj = modalItems[index],
+        state = (item.dataset.state === 'true'),
+        newState = !state
+
+    if (state) {
+        item.classList.add('button--disable')
+    } else {
+        item.classList.remove('button--disable')
+    }
+
+    item.dataset.state = newState
+    obj.settings.execute = newState
+
+    if (newState) {
+        obj.createBlock(`
+        <div class="slide-block" id="slideBlockOverlay">
+            <div class="slide-block slide-block--center" id="slideBlockModalComplex">
+                <div class="modal" id="modal">
+                    <div class="button" id="buttonClose">
+                         <span class="button__text">CLICK ME</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+            `, 'header')
+        obj.elementListener()
+    }
+}
+
+if (btnsModal) {
+    btnsModal.forEach(function (item) {
+
+        item.addEventListener("click", () => {
+            setState(item)
+        })
+
+        const state = (item.dataset.state === 'true')
+
+        modalItems.push(new SlideBlock({
+            elementID: 'slideBlockModalComplex',
+            destroy: true,
+            delayAll: 100,
+            overlay: true,
+            overlayDisables: false,
+            execute: state,
+            elementsActivateID: [`modalComplex_${item.dataset.item}`],
+            elementsDisableID: ['buttonClose'],
+        }))
+
+    })
+}
 
 const slideBlockUp = new SlideBlock({
     elementID: 'slideBlockUp',
--- app/js/slideBlock.js
+++ app/js/slideBlock.js
@@ -7,20 +7,49 @@
             overlayDisables: true,
             startMove: false,
             delayStart: 500,
+            delayAll: 0,
+            destroy: false,
             elementsActivateID: [],
-            elementsDisableID: []
+            elementsDisableID: [],
+            execute: function () { return true }
         }
         Object.assign(settings, options)
         const element = doc.getElementById(settings.elementID)
         this.element = element
         this.settings = settings
+        this._destroyed = false
         this.elementListener()
         this.startMove()
     }
 
+    SlideBlock.prototype.createBlock = function (textHTML, className) {
+        const element = doc.querySelector(className)
+        if (element) {
+            element.insertAdjacentHTML("afterEnd", textHTML)
+            this._destroyed = false
+        }
+    }
+
+    SlideBlock.prototype.destroy = function () {
+        const settings = this.settings,
+            element = doc.getElementById(settings.elementID),
+            parentElement = element.parentElement
+
+        if (element) {
+            if (parentElement.id === 'slideBlockOverlay') {
+                parentElement.innerHTML = ''
+                parentElement.remove()
+            } else {
+                element.parentNode.removeChild(element)
+            }
+        }
+        this._destroyed = true
+    }
+
     SlideBlock.prototype.disableBlock = function () {
         const settings = this.settings,
-            element = this.element
+            element = this.element || doc.getElementById(settings.elementID)
+
         if (element) {
             element.classList.remove('slide-block--active')
             if (settings.overlay) {
@@ -30,26 +59,40 @@
                     slideBlockOverlay.classList.remove('slide-block--overlay')
                 }
             }
+            if (settings.destroy && !this._destroyed) {
+                setTimeout(function () {
+                    this.destroy()
+                }.bind(this), settings.delayAll)
+            }
         }
     }
 
     SlideBlock.prototype.activateBlock = function () {
         const settings = this.settings,
-            element = this.element
+            element = this.element || doc.getElementById(settings.elementID),
+            resultExecute = (typeof settings.execute === "function" ? settings.execute() : settings.execute)
+
+        if (!resultExecute) { return false }
+
         if (element) {
-            element.classList.add('slide-block--active')
-            if (settings.overlay) {
-                const slideBlockOverlay = doc.getElementById('slideBlockOverlay')
-                if (slideBlockOverlay) {
-                    slideBlockOverlay.classList.add('slide-block--overlay')
-                    doc.body.classList.add('slide-block--overflow')
-                    if (settings.overlayDisables) {
-                        slideBlockOverlay.addEventListener('click', function () {
-                            this.disableBlock()
-                        }.bind(this))
+            const addClasses = function () {
+                element.classList.add('slide-block--active')
+                if (settings.overlay) {
+                    const slideBlockOverlay = doc.getElementById('slideBlockOverlay')
+                    if (slideBlockOverlay) {
+                        slideBlockOverlay.classList.add('slide-block--overlay')
+                        doc.body.classList.add('slide-block--overflow')
+                        if (settings.overlayDisables) {
+                            slideBlockOverlay.addEventListener('click', function () {
+                                this.disableBlock()
+                            }.bind(this))
+                        }
                     }
                 }
-            }
+            }.bind(this)
+            setTimeout(function () {
+                addClasses()
+            }.bind(this), settings.delayAll)
         }
     }
 
